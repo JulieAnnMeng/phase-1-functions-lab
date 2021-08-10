2392baa1e421e7a9caddce6ecaa81d4c2b9bec40
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-09 21:14:14 -0400

    commit tree b9d9d0180ebb694f2da10f07aefb6fe099a327e1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const scubersHq = 42;
+const blockFeet = 264;
+
+function distanceFromHqInBlocks(block) {
+    return Math.abs(scubersHq - block);
+}
+
+function distanceFromHqInFeet(block) {
+    let feetTotal = distanceFromHqInBlocks(block) * blockFeet;
+    return feetTotal;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocksTraveled = Math.abs(start - destination);
+    let feetTraveled = blocksTraveled * blockFeet;
+    return feetTraveled
+}
+
+function calculatesFarePrice(start, destination){
+    let farePrice;
+    let feetTraveled = distanceTravelledInFeet(start, destination);
+
+    if (feetTraveled <= 400){
+        farePrice = 0;
+    } else if (feetTraveled > 400 && feetTraveled < 2000) {
+        farePrice = (feetTraveled - 400) * .02;
+    } else if (feetTraveled > 2000 && feetTraveled < 2500) {
+        farePrice = 25;
+    } else {farePrice = 'cannot travel that far'}
+
+    return farePrice
+}
